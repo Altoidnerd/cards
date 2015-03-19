03dc6f992713bb92587f7a3b4c94965aed02d316
altoidnerd <altoidnerd.btc@gmail.com>
2015-03-19 01:17:18 -0400

    BankRoll() class absorbed into Player()

--- src/bet.py
+++ src/bet.py
@@ -4,57 +4,53 @@ import itertools
 import random
 from pokerHands import *
 
-class BankRoll(object):
-    def __init__(self, balance=None):
-        if (balance is None):
-            balance=0
-        self.balance=balance
-    def __str__(self):
-        return str(self.balance)
-    def __repr__(self):
-        return self.__str__()
-    def bet(self, amount, pot):
-        if self.balance-amount<0:
-            raise ValueError("Bet exceeds balance!")
-        else:
-            self.balance-=amount
-            pot.balance+=amount
-            print("The pot is now %s" % pot.balance)
-            return pot.balance
-    def credit(self, amount):
-        self.balance+=amount
-        return self.balance
-    def debit(self, amount):
-        self.balance-=amount
-        return self.balance
-
 class Table(object):
     def __init__(self, players=None, pot=None):
         if players is none:
-            self.players=None
+            self.players = None
         else:
-            self.players=players 
+            self.players = players 
         if pot is None:
-            self.pot=BankRoll()
+            self.pot = BankRoll()
     def setLimit(self, lim):
-        self.limit=lim
+        self.limit = lim
 
 class Player(object):
     def __init__(self, name, credit=None, cards=None):
-        self.name=name
+        self.name = name
         if credit is None:
-            self.bankroll=BankRoll()
+            self.balance = 0
         else:
-            self.bankroll=BankRoll(credit)
+            self.balance = credit
         if cards is None:
-            self.cards=OrderedCards()
-            self.cards_list=[]
+            self.cards = OrderedCards()
+            self.cards_list = []
         else:
-            self.cards=OrderedCards(cards)
-            self.cards_list=cards.cards_list()
+            self.cards = OrderedCards(cards)
+            self.cards_list = cards.cards_list()
         self.action=False
+
     def __str__(self):
-        return "name: "+self.name+", balance: "+str(self.bankroll)+", cards: "+str(self.cards)
+        return "name: "+self.name+", balance: "+str(self.balance)+", cards: "+str(self.cards_list)
     def __repr__(self):
         return self.__str__()
+    def bet(self, amount, pot):
+        if self.balance - amount < 0:
+            raise ValueError("Bet exceeds balance!")
+        else:            
+            self.balance -= amount
+            pot.balance += amount
+            print("The pot is now %s" % pot.balance)
+            return pot.balance
+
+    def credit(self, amount):
+        self.balance += amount
+        return self
+
+    def debit(self, amount):
+        if self.balance - amount < 0:
+            raise ValueError("Debit exceeds balance!")
+        else:
+            self.balance -= amount
+            return self
 
