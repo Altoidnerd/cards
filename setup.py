5b83f4277c4aef368f0bf7e7cee64b15d847c029
altoidnerd <altoidnerd.btc@gmail.com>
2015-03-19 02:27:22 -0400

    src/ --> cards/; added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cards',
+    version='0.1.1',
+    description='Cards and poker backend',
+    author='Allen Majewski',
+    author_email='altoidnerd.btc@gmail.com',
+    url='',
+    packages=['cards'],
+    )
